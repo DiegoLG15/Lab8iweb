55303ad461a645c1ac1b65e7e5fb65ec72abed96
abigail200616 <116697266+abigail200616@users.noreply.github.com>
2022-11-09 18:52:39 -0500

    Abigail Clase Enemigos Beans

---
+++ Lab8iweb/src/main/java/Beans/ClaseEnemigos.java
@@ -0,0 +1,25 @@
+package Beans;
+
+public class ClaseEnemigos {
+
+    private int idClase;
+    private String nombre;
+
+
+    public int getIdClase() {
+        return idClase
+    }
+
+    public void setIdClase(int idClase) {
+        this.idClase = idClase
+    }
+
+    public String getNombre() {
+        return nombre
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre
+    }
+
+}
